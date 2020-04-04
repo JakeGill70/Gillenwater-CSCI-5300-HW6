ef5f77004ea7323f4ed9e7cee5fa84cfaf353a75
Jake Gillenwater <JakeGill70@gmail.com>
2020-04-04 17:29:49 -0400

    Changed 2a's recursive function calls to be more explicit

--- js/problem2a.js
+++ js/problem2a.js
@@ -29,7 +29,6 @@ var problem2a = {
     // Recursive determines the number at the specied index within the lucas series
     _getLucasNumber : function(index){
 
-        
         // Error checking for timeout
         if(endTime - Date.now() <= 0){
             throw("Exception: maxTime exceeded");
@@ -48,8 +47,8 @@ var problem2a = {
             valueAtIndex = 1;
         }
         else {
-            let gln1 = _getLucasNumber(index-1); // Get the previous number in the lucas series
-            let gln2 = _getLucasNumber(index-2); // Get the previous previous number in the lucas series
+            let gln1 = problem2a._getLucasNumber(index-1); // Get the previous number in the lucas series
+            let gln2 = problem2a._getLucasNumber(index-2); // Get the previous previous number in the lucas series
             valueAtIndex = gln1[0] + gln2[0]; // Determine the value at this index
             timesCalled += gln1[1] + gln2[1]; // Add up the number of times this method was called
         }
